9b4ba801d54385cc570901be958a0503a612d190
Andreas Hauser <andreas@creative-memory.de>
2015-09-14 12:39:06 +0200

    Remove not yet existing function delaration.

--- src/ffindex_posix_search.h
+++ src/ffindex_posix_search.h
@@ -4,6 +4,4 @@ ffindex_index_t* ffindex_index_as_tree(ffindex_index_t* index);
 
 ffindex_index_t* ffindex_tree_unlink(ffindex_index_t* index, char* name_to_unlink);
 
-ffindex_index_t* ffindex_unlink_entries(ffindex_index_t* index, char** sorted_names_to_unlink, size_t n_names);
-
 int ffindex_tree_write(ffindex_index_t* index, FILE* index_file);
